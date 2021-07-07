a2577e31ecc053256abcc4c741c0c6a335833fa4
Mohamed <mtaha@pp.gov.qa>
2021-07-07 21:06:58 +0300

    initial commit

---
+++ FrontEnd/HR-UI-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HR_UI_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
